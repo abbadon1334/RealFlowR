e2d6778b748775f6a8e2bd8f0d1a5522efae8bab
Francesco Danti <fdanti@gmail.com>
2021-01-11 05:39:08 +0100

    Add desccriptive log to sample

--- FlowR.Sample/Application.cs
+++ FlowR.Sample/Application.cs
@@ -8,6 +8,7 @@ namespace FlowR.Sample
     public class Application : Library.Client.Application
     {
         protected int counter = 0;
+        protected Div rightColumn;
 
         public Application(string connectionId, IClientProxy client) : base(connectionId, client)
         {
@@ -15,7 +16,13 @@ namespace FlowR.Sample
 
         protected override void OnStart(Root rootElement)
         {
-            var container = RootElement
+            var masterContainer = rootElement.Add(new Div()).SetAttribute("class", "container-fluid");
+            var containerRow = masterContainer.Add(new Div()).SetAttribute("class", "row");
+            
+            var leftColumn = containerRow.Add(new Div()).SetAttribute("class", "col");
+            rightColumn = containerRow.Add(new Div()).SetAttribute("class", "col") as Div;
+            
+            var container = leftColumn
                 .Add(new Div())
                 .SetAttribute("class", "card");
 
@@ -26,13 +33,13 @@ namespace FlowR.Sample
             
             AddTimer(1, (sender, args) =>
             {
-                cardHeaderTime.SetText($"Server Time :{DateTime.Now.ToString("O")}");
+                cardHeaderTime.SetText($"ApplicationTimer which update Text every (1ms) with server Time : {DateTime.Now.ToString("O")}");
             });
             
             var cardHeader = container
                 .Add(new Div())
                 .SetAttribute("class", "card-header")
-                .SetText("Right");
+                .SetText("Children 0");
 
             var cardBody = container
                 .Add(new Div())
@@ -40,8 +47,6 @@ namespace FlowR.Sample
 
             var cardText = cardBody
                 .Add(new Div());
-
-            cardText.SetText("card-text");
 
             var buttonAdd1000 = cardBody
                 .Add(new Button())
@@ -57,7 +62,7 @@ namespace FlowR.Sample
                 .Add(new Button())
                 .SetAttribute("class", "btn btn-success")
                 .SetText(
-                    "client click -> server ask client innerHTML -> client return innerHtml -> server add a point to innerHTML");
+                    "2 way communication Test");
 
 
             var testContainer = RootElement
@@ -66,7 +71,13 @@ namespace FlowR.Sample
 
             buttonAdd1000.On("click", delegate
             {
-                buttonAdd1000.SetProperty("value", "test");
+                ClearLogActions();
+                AddLogAction("button Add1000 clicked on client");
+                AddLogAction("JS Client : notify server that a click event happen on DomNode with UID : " + buttonAdd1000.GetUuid());
+                AddLogAction("SRV : search DomNode registry for UID : " + buttonAdd1000.GetUuid());
+                AddLogAction("SRV : trigger defined callback on click");
+                AddLogAction("SRV : which will fire 1000 create event on client side and for every call update the children count");
+                //buttonAdd1000.SetProperty("value", "test");
                 for (var x = 0; x < 1000; x++)
                 {
                     var count = testContainer.GetChildrenCount();
@@ -84,6 +95,13 @@ namespace FlowR.Sample
 
             buttonRemove.On("click", delegate
             {
+                ClearLogActions();
+                AddLogAction("button Remove clicked on client");
+                AddLogAction("JS Client : notify server that a click event happen on DomNode with UID : " + buttonRemove.GetUuid());
+                AddLogAction("SRV : search DomNode registry for UID : " + buttonRemove.GetUuid());
+                AddLogAction("SRV : trigger defined callback on click");
+                AddLogAction("SRV : which will fire 1000 remove child event on client side and for every call update the children count");
+
                 while (testContainer.GetChildrenCount() != 0)
                 {
                     testContainer.Remove(testContainer.GetLastChild());
@@ -95,9 +113,27 @@ namespace FlowR.Sample
 
             buttonTestResponse.On("click", async delegate
             {
+                ClearLogActions();
+                AddLogAction("button Add1000 clicked on client");
+                AddLogAction("JS Client : notify server that a click event happen on DomNode with UID : " + buttonTestResponse.GetUuid());
+                AddLogAction("SRV : search DomNode registry for UID : " + buttonTestResponse.GetUuid());
+                AddLogAction("SRV : trigger defined callback on click");
+                AddLogAction("SRV : Ask client for JS property `innerHTML`");
+                AddLogAction("JS : Response with `innerHTML` value");
+                AddLogAction("SRV : Set client " + buttonTestResponse.GetUuid() + " JS property `innerHTML` with the same label adding a point at the end");
+
                 var label = await buttonTestResponse.GetProperty("innerHTML");
                 buttonTestResponse.SetProperty("innerHTML", $"{label}."); // add a point as an example of the workflow on every call
             });
+        }
+
+        void ClearLogActions()
+        {
+            rightColumn.SetText("");
+        }
+        void AddLogAction(string log)
+        {
+            rightColumn.SetText(rightColumn.GetText() + log + "<br/>");
         }
     }
 }
