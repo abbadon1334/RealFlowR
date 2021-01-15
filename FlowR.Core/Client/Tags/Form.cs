517731879e4ebb1096e874731297b5630ad34d82
Francesco Danti <fdanti@gmail.com>
2021-01-16 00:23:08 +0100

    Wip Form Element

---
+++ FlowR.Core/Client/Tags/Form.cs
@@ -0,0 +1,23 @@
+
+using FlowR.Library.Node;
+
+namespace FlowR.Library.Client.Tags
+{
+    /// <summary>
+    /// Tag Form
+    /// </summary>
+    /// <example>
+    ///
+    /// Form.BindControl(fldName);
+    /// 
+    /// Form.BindControls(fldSurname, fldAddress);
+    ///
+    /// Form.submit(); // for all binded controls, will be called Collect()
+    /// to retrieve the actual value
+    /// @todo add a loading or something during this type of operations 
+    /// </example>
+    public class Form : DomNode
+    {
+
+    }
+}
