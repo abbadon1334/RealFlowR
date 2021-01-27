fba9e95696de696ca82530fd62b09bae58e47463
Francesco Danti <fdanti@gmail.com>
2021-01-28 00:09:00 +0100

    move enums to single file

---
+++ FlowR.UI/Enums.cs
@@ -0,0 +1,265 @@
+namespace FlowR.UI
+{
+    public class Enums
+    {
+
+        /// <summary>
+        ///     Border radius enum
+        /// </summary>
+        public enum BorderRadiusEnum
+        {
+            /// <summary>
+            ///     radius top
+            /// </summary>
+            Top,
+            /// <summary>
+            ///     radius right
+            /// </summary>
+            End,
+            /// <summary>
+            ///     radius bottom
+            /// </summary>
+            Bottom,
+            /// <summary>
+            ///     radius left
+            /// </summary>
+            Start,
+            /// <summary>
+            ///     radius all circle
+            /// </summary>
+            Circle,
+            /// <summary>
+            ///     radius all pill
+            /// </summary>
+            Pill,
+        }
+
+        /// <summary>
+        ///     Viewports enum
+        /// </summary>
+        public enum BreakpointsEnum
+        {
+            /// <summary>
+            ///     SM
+            /// </summary>
+            Sm,
+            /// <summary>
+            ///     MD
+            /// </summary>
+            Md,
+            /// <summary>
+            ///     LG
+            /// </summary>
+            Lg,
+            /// <summary>
+            ///     XL
+            /// </summary>
+            Xl,
+            /// <summary>
+            ///     XXL
+            /// </summary>
+            Xxl,
+            /// <summary>
+            ///     FLUID
+            /// </summary>
+            Fluid,
+        }
+
+        /// <summary>
+        ///     Color enum
+        /// </summary>
+        public enum ColorsEnum
+        {
+            /// <summary>
+            ///     primary color
+            /// </summary>
+            Primary,
+
+            /// <summary>
+            ///     secondary color
+            /// </summary>
+            Secondary,
+
+            /// <summary>
+            ///     success color
+            /// </summary>
+            Success,
+
+            /// <summary>
+            ///     danger color
+            /// </summary>
+            Danger,
+
+            /// <summary>
+            ///     warning color
+            /// </summary>
+            Warning,
+
+            /// <summary>
+            ///     info color
+            /// </summary>
+            Info,
+
+            /// <summary>
+            ///     light color
+            /// </summary>
+            Light,
+
+            /// <summary>
+            ///     dark color
+            /// </summary>
+            Dark,
+
+            /// <summary>
+            ///     white color
+            /// </summary>
+            White,
+
+            /// <summary>
+            ///     transparent color
+            /// </summary>
+            Transparent,
+        }
+
+        /// <summary>
+        ///     Text color Enum
+        /// </summary>
+        public enum TextColorEnum
+        {
+            /// <summary>
+            ///     primary color
+            /// </summary>
+            Primary,
+
+            /// <summary>
+            ///     secondary color
+            /// </summary>
+            Secondary,
+
+            /// <summary>
+            ///     success color
+            /// </summary>
+            Success,
+
+            /// <summary>
+            ///     danger color
+            /// </summary>
+            Danger,
+
+            /// <summary>
+            ///     warning color
+            /// </summary>
+            Warning,
+
+            /// <summary>
+            ///     info color
+            /// </summary>
+            Info,
+
+            /// <summary>
+            ///     light color
+            /// </summary>
+            Light,
+
+            /// <summary>
+            ///     dark color
+            /// </summary>
+            Dark,
+
+            /// <summary>
+            ///     white color
+            /// </summary>
+            White,
+
+            /// <summary>
+            ///     transparent color
+            /// </summary>
+            Transparent,
+
+            /// <summary>
+            ///     body color
+            /// </summary>
+            Body,
+
+            /// <summary>
+            ///     muted color
+            /// </summary>
+            Muted,
+        }
+
+        /// <summary>
+        ///     Flexbox enum align
+        /// </summary>
+        public class FlexboxAlignsEnum
+        {
+            /// <summary>
+            ///     Flexbox align content
+            /// </summary>
+            public enum Content
+            {
+                /// <summary>
+                ///     Start
+                /// </summary>
+                Start,
+                /// <summary>
+                ///     Center
+                /// </summary>
+                Center,
+                /// <summary>
+                ///     End
+                /// </summary>
+                End,
+                /// <summary>
+                ///     Around
+                /// </summary>
+                Around,
+                /// <summary>
+                ///     Between
+                /// </summary>
+                Between,
+                /// <summary>
+                ///     Evenly
+                /// </summary>
+                Evenly,
+            }
+
+            /// <summary>
+            ///     Flexbox align items
+            /// </summary>
+            public enum Items
+            {
+                /// <summary>
+                ///     Start
+                /// </summary>
+                Start,
+                /// <summary>
+                ///     Center
+                /// </summary>
+                Center,
+                /// <summary>
+                ///     End
+                /// </summary>
+                End,
+            }
+
+            /// <summary>
+            ///     Flexbox align self
+            /// </summary>
+            public enum Self
+            {
+                /// <summary>
+                ///     Start
+                /// </summary>
+                Start,
+                /// <summary>
+                ///     Center
+                /// </summary>
+                Center,
+                /// <summary>
+                ///     End
+                /// </summary>
+                End,
+            }
+        }
+    }
+}
