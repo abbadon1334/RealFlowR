73b98dd94551afb5ecdfe9663b53139eba7b8203
Francesco Danti <fdanti@gmail.com>
2021-02-10 21:59:11 +0100

    Fix codeclimate errors

--- FlowR.UI/Forms/Controls/IBootstrapControl.cs
+++ FlowR.UI/Forms/Controls/IBootstrapControl.cs
@@ -1,5 +1,8 @@
 namespace FlowR.UI.Forms.Controls
 {
+    /// <summary>
+    ///     Interface definition for bootstrap elements. 
+    /// </summary>
     public interface IBootstrapControl
     {
     }
